fe72fedbb5126129a7d4ebafa1b7eb3f7b166c7d
Martin Honnen <martin.honnen@gmx.de>
2022-06-06 14:01:18 +0200

    Projektdateien hinzufügen.

---
+++ MAUIXdmViewTest1/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIXdmViewTest1;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MAUIXdmViewTest1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MAUIXdmViewTest1;
+using Saxon.Api;
+
+public partial class MainPage : ContentPage
+{
+	private static Processor processor = new Processor();
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+        var values = new string[] { "foo", "bar", "baz" };
+
+        //var values = new XdmItem[] { new XdmAtomicValue("foo"), new XdmAtomicValue(3), new XdmAtomicValue(true) };
+
+        //var xqueryCompiler = processor.NewXQueryCompiler();
+        //xqueryCompiler.BaseUri = new Uri("urn:from-string");
+        //var xqueryExecutable = xqueryCompiler.Compile("(1 to 5) ! <item>Item {.}</item>");
+        //var xqueryEvaluator = xqueryExecutable.Load();
+
+        //var xdmValue = xqueryEvaluator.Evaluate();
+
+        XdmView.ItemsSource = values;
+
+    }
+
+}
+
